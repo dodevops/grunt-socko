d1b44d9f18ffa364b92df36945a3d4bc1ddec36e
Dennis Ploeger <dennis.ploeger@kps.com>
2018-01-22 15:32:18 +0100

    Updated files

--- tasks/socko.js
+++ tasks/socko.js
@@ -1,7 +1,4 @@
 "use strict";
-/**
- * Grunt Socko Task
- */
 const sockoTaskFunction_1 = require("../lib/sockoTaskFunction");
 module.exports = function (grunt) {
     grunt.registerMultiTask('socko', 'Grunt-Plugin for SOCKO!', sockoTaskFunction_1.taskFunction);
