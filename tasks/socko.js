5331f95b7e535e2c7b1071cd11f5bd0942579d10
Dennis Ploeger <dennis.ploeger@kps.com>
2018-02-15 09:01:07 +0100

    Error in the baseman baseline. Missing ignores

--- tasks/socko.js
+++
@@ -1,6 +0,0 @@
-"use strict";
-const sockoTaskFunction_1 = require("../lib/sockoTaskFunction");
-module.exports = function (grunt) {
-    grunt.registerMultiTask('socko', 'Grunt-Plugin for SOCKO!', sockoTaskFunction_1.taskFunction);
-};
-//# sourceMappingURL=socko.js.map
